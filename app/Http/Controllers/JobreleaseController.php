17c634b6ac95cbbb09184666d20be532489b5118
beta0221 <beta0221@gmail.com>
2019-07-16 00:53:47 +0800

    .

--- app/Http/Controllers/JobreleaseController.php
+++ app/Http/Controllers/JobreleaseController.php
@@ -11,7 +11,7 @@ class JobreleaseController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->except(['getJobRelease']);
+        $this->middleware('auth')->except(['getJobRelease','billboard']);
     }
     /**
      * Display a listing of the resource.
